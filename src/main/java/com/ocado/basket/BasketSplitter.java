d73d2d1d85ce2a3ae03f451db095bddb87852713
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-04-06 12:55:52 +0200

    Add json to map mapping method and a constructor

--- src/main/java/com/ocado/basket/BasketSplitter.java
+++ src/main/java/com/ocado/basket/BasketSplitter.java
@@ -1,9 +1,12 @@
 package com.ocado.basket;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,13 +15,28 @@ import java.util.Map;
 @Setter
 public class BasketSplitter
 {
-    private String absolutePathToConfigFile;
+    private Map<String, List<String>> config;
     public BasketSplitter(String absolutePathToConfigFile) {
+        config = mapJsonToConfigMap(absolutePathToConfigFile);
+    }
 
+    private Map<String, List<String>> mapJsonToConfigMap(String absolutePathToConfigFile) {
+        Map<String, List<String>> config;
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            config = mapper.readValue(
+                    new File(absolutePathToConfigFile),
+                    new TypeReference<>() {});
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return config;
     }
 
     public Map<String, List<String>> split(List<String> items) {
-        
+        return new HashMap<>();
     }
 }
 
