39a52fd76cf95ee0067cca12cf79a586b4bd9536
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-04-06 23:00:58 +0200

    Implement splitting algorithm, add handling empty config files to the mapper

--- src/main/java/com/ocado/basket/BasketSplitter.java
+++ src/main/java/com/ocado/basket/BasketSplitter.java
@@ -7,9 +7,7 @@ import lombok.Setter;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Getter
 @Setter
@@ -32,11 +30,58 @@ public class BasketSplitter
             throw new RuntimeException(e);
         }
 
-        return config;
+        if (!config.isEmpty()) {
+            return config;
+        }
+        else {
+            throw new IllegalStateException("Config file is empty");
+        }
+    }
+
+    private void sortDeliveryMap(Map<String, Integer> map) {
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        map.clear();
+        for (Map.Entry<String, Integer> entry : list) {
+            map.put(entry.getKey(), entry.getValue());
+        }
     }
 
     public Map<String, List<String>> split(List<String> items) {
-        return new HashMap<>();
+        Map<String, Integer> deliveryCount = getDeliveryCount(items);
+        Map<String, List<String>> basketSplit = new LinkedHashMap<>();
+        for (String item : items) {
+            boolean found = false;
+            for (String option : deliveryCount.keySet()) {
+                if (config.get(item).contains(option)) {
+                    if (!found) {
+                        List<String> list = basketSplit.getOrDefault(option, new ArrayList<>());
+                        list.add(item);
+                        basketSplit.put(option, list);
+                        found = true;
+                    }
+                    else {
+                        Integer count = deliveryCount.get(option);
+                        deliveryCount.put(option, count-1);
+                    }
+                }
+            }
+            sortDeliveryMap(deliveryCount);
+        }
+        return basketSplit;
+    }
+
+    private Map<String, Integer> getDeliveryCount(List<String> items) {
+        Map<String, Integer> deliveryCount = new LinkedHashMap<>();
+        for (String item : items) {
+            for (String option : config.get(item)) {
+                Integer count = deliveryCount.getOrDefault(option, 0);
+                deliveryCount.put(option, count + 1);
+            }
+        }
+        sortDeliveryMap(deliveryCount);
+        return deliveryCount;
     }
 }
+
 
